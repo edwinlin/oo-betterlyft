ae354adaa2b62ac442abd0407f6264ebf8fc44a2
edwinlin <edwinlin@gmail.com>
2019-01-14 12:05:51 -0500

    done

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,40 @@
+class Driver
+	attr_accessor :name
+	@@all = []
+	@@distance = 0
+	
+	def self.all
+		@@all
+	end
 
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def rides
+		Ride.all.select do |ride|
+			ride.driver == self
+		end
+	end
+
+	def total_distance
+		dist = rides.inject(0) do |sum, ride|
+			sum + ride.distance
+		end
+		dist
+	end
+
+	def passenger_names
+		rides.map do |ride|
+			ride.passenger
+		end
+	end
+
+	def self.mileage_cap(distance)
+		self.all.select do |driver|
+			driver.total_distance > distance
+		end
+	end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,4 +7,24 @@ end
 
 # Put your variables here~!
 
+dave = Driver.new("Dave")
+eric = Driver.new("Eric")
+rob = Driver.new("Rob")
+
+lois = Passenger.new("Lois")
+bob = Passenger.new("Bob")
+rachel = Passenger.new("Rachel")
+
+#Ride (Driver, Passenger, Distance)
+ride1 = Ride.new(dave, rachel, 10)
+ride2 = Ride.new(eric, bob, 5)
+ride3 = Ride.new(rob, lois, 8)
+ride4 = Ride.new(dave, bob, 2)
+ride5 = Ride.new(dave, lois, 4)
+ride6 = Ride.new(eric, lois, 8)
+ride7 = Ride.new(rob, rachel, 8)
+ride8 = Ride.new(dave, rachel, 100)
+
+
+
 binding.pry
